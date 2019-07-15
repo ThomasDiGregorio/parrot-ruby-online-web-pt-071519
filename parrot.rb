803c3b9d5f07fd7e7bd3ae5218e7adc9e8b0eee2
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-15 20:24:27 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  parrot_name = "Polly"
+   talking_bird = "The blue car is very fast"
+  saying = "Squawk!"
+end
